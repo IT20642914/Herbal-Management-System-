ccb26d2789ac98363e9e43a53c7ec33afe8414e2
avishka05 <88711826+avishka05@users.noreply.github.com>
2023-04-19 00:15:32 +0530

    initial project

---
+++ herbify/src/constants/constants.js
@@ -0,0 +1,14 @@
+export const headerNavigations = [
+    {
+      key: 1,
+      name: 'Home',
+      path: '/',
+      selected: true,
+    },
+    { key: 2, name: 'About Us', path: '/about', selected: false },
+    { key: 3, name: 'Services', path: 'services', selected: false },
+    { key: 4, name: 'Careers', path: '/careers', selected: false },
+    { key: 5, name: 'Case Studies', path: '/case-studies', selected: false },
+    { key: 6, name: 'Contact Us', path: '/contact', selected: false },
+  ];
+    
