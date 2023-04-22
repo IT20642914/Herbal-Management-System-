aba5222b0118fc9e13e6c40b95ce92f07899a085
avishka05 <88711826+avishka05@users.noreply.github.com>
2023-04-23 01:15:41 +0530

    setting up redux

--- herbify/src/App.jsx
+++ herbify/src/App.jsx
@@ -5,9 +5,12 @@ import PublicRoute from './routes/PublicRoutes';
 import  HomePage from './Components/HomePage/Homepage';
 import StorePage from './Components/StorePage/StorePage';
 import AbountPage from './Components/aboutPage/AbountPage';
+import { ToastContainer } from 'react-toastify';
+import { useSelector } from 'react-redux'
 function App() {
   return (
     <Router>
+       <ToastContainer/>
     {/* <ScrollToTop /> */}
     <Routes>
       <Route path='/' element={<PublicRoute />}>
--- herbify/src/index.js
+++ herbify/src/index.js
@@ -6,12 +6,21 @@ import { MUItheme } from './styles/theme';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'react-toastify/dist/ReactToastify.css';
+
+import { store,persistore } from './redux/stores';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store} >
+    <PersistGate persistor={persistore}>
         <ThemeProvider theme={MUItheme}>
     <App />
     </ThemeProvider>
+    </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 reportWebVitals();
